5d7fafccc039b19145ef645ae37f3bd37c657187
R S K <66393045+mn9-29@users.noreply.github.com>
2023-08-28 23:09:56 +0300

    update sql

--- data.sql
+++ data.sql
@@ -1,12 +1,16 @@
-CREATE TABLE blips (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255),
-    sprite INT,
-    coord_x FLOAT,
-    coord_y FLOAT,
-    coord_z FLOAT,
-    opentime INT,
-    closetime INT,
-    opencolor VARCHAR(255),
-    closecolor VARCHAR(255)
-);
+CREATE TABLE `blips` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+    `sprite` INT(11) NOT NULL,
+    `position` LONGTEXT NOT NULL COLLATE 'utf8mb4_bin',
+    `openTime` INT(11) NOT NULL DEFAULT '8',
+    `closeTime` INT(11) NOT NULL DEFAULT '20',
+    `openColor` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+    `closeColor` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `name` (`name`) USING BTREE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=17
+;
